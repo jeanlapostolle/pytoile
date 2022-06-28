c9e63fcdc2f90db6965ecbec9b8fe40fef43eac9
Jean Lapostolle <lapostolle.jean@gmail.com>
2022-06-28 15:14:06 +0200

    Use Jinja templating

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+
 class Site():
-    template = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{title}</title>
-</head>
-<body>
-</body>
-</html>"""
-    def __init__(self, title="Pytoile"):
-        self.title = title
+    def __init__(self, title="Pytoile", template="templates/base.html"):
+        self.variables = {"title" : title}
 
     def generate(self, path="index.html"):
-        with open(path, 'w+') as f:
-            f.write(self.template.format(title = self.title))
+        root = os.path.dirname(os.path.abspath(__file__))
+        templates_dir = os.path.join(root, 'templates')
+        env = Environment(loader=FileSystemLoader(templates_dir))
+        template = env.get_template('menu.html')
+
+        filename = os.path.join(root, 'html', 'index.html')
+        with open(filename, 'w') as fh:
+            rend = template.render(
+                is_menu=True,
+                menu_items=[{'name': "Accueil", 'link': "/accueil"}, {'name': "Blog", 'link': "/blog"}]
+            )
+            fh.write(rend)
 
 
-if __name__=="__main__":
-    site = Site()
+if __name__ == "__main__":
+    site = Site(template="templates/menu.html")
     site.generate()
